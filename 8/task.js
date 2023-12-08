fc4a3bb5c384ee115fc6592c3fd82ff7936eafee
Alexander Francis Månebjælke Henry <10469240+afmhenry@users.noreply.github.com>
2023-12-08 14:22:42 +0100

    no clue why this workds...guess my other solution was stuck in a circular loop?

--- 8/task.js
+++ 8/task.js
@@ -1,7 +1,9 @@
 import fs from "fs";
 const day = 8;
 const map = {};
+const newMap = {};
 let instructions;
+let iterations = 1;
 
 function runTask(stage, filename, task, expected) {
   console.log(`\nTask ${stage} - ${filename}`);
@@ -14,7 +16,7 @@ function runTask(stage, filename, task, expected) {
 }
 runTask("1", "sample", firstTask, 2);
 runTask("1", "sample-2", firstTask, 6);
-//runTask("1", "full", firstTask, 254024898);
+runTask("1", "full", firstTask, 18673);
 // runTask("2", "sample", secondTask, 5905);
 // runTask("2", "full", secondTask, 254115617);
 function firstTask(filename) {
@@ -36,20 +38,17 @@ function firstTask(filename) {
   });
   let i = 0;
   // eslint-disable-next-line no-constant-condition
-  Object.entries(map).forEach(([key, value]) => {
-    let prevLocation = key;
-    for (i = 0; i < instructions.length; i++) {
-      prevLocation = map[prevLocation][instructions[i]];
-      map[key][instructions.slice(0, i + 1).join("")] = prevLocation;
+  let prevLocation = "AAA";
+  let result = 0;
+  for (i = 0; true; i++) {
+    let newLocation = map[prevLocation][instructions[i % instructions.length]];
+    newMap[prevLocation] ||= [];
+    newMap[prevLocation].push({ i: i + 1, next: newLocation });
+    if (newLocation === "ZZZ") {
+      result = i + 1;
+      break;
     }
-  });
-  console.log(map);
-  // while (true) {
-  //   currentLocation = map[currentLocation].X;
-  //   if (currentLocation === "ZZZ") break;
-  //   i++;
-  // }
-  // return (i * instructions.length) / 2;
+    prevLocation = newLocation;
+  }
+  return result;
 }
-
-function enrichLocation(currentLocation, i) {}

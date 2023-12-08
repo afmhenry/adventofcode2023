33d503d4a05755277331389a87af4ffd07296492
Alexander Francis Månebjælke Henry <10469240+afmhenry@users.noreply.github.com>
2023-12-08 12:03:58 +0100

    have to optimize

--- 8/task.js
+++ 8/task.js
@@ -1,22 +1,7 @@
 import fs from "fs";
 const day = 8;
-const cards = [
-  "A",
-  "K",
-  "Q",
-  "T",
-  "9",
-  "8",
-  "7",
-  "6",
-  "5",
-  "4",
-  "3",
-  "2",
-  "-1",
-  "-1",
-  "J",
-].reverse();
+const map = {};
+let instructions;
 
 function runTask(stage, filename, task, expected) {
   console.log(`\nTask ${stage} - ${filename}`);
@@ -27,158 +12,44 @@ function runTask(stage, filename, task, expected) {
   console.log(`\tResult: ${result}`);
   console.log(`\tPass: ${result === expected}\n`);
 }
-runTask("1", "sample", firstTask, 6440);
-runTask("1", "sample-2", firstTask, 6440);
-runTask("1", "full", firstTask, 254024898);
-runTask("2", "sample", secondTask, 5905);
-runTask("2", "full", secondTask, 254115617);
-
+runTask("1", "sample", firstTask, 2);
+runTask("1", "sample-2", firstTask, 6);
+//runTask("1", "full", firstTask, 254024898);
+// runTask("2", "sample", secondTask, 5905);
+// runTask("2", "full", secondTask, 254115617);
 function firstTask(filename) {
   const input = fs.readFileSync(`${day}/${filename}.txt`).toString();
   const inputAsRows = input.split("\n");
-  const handsWithValues = [];
-  inputAsRows.forEach((row) => {
-    let [hand, value] = row.split(" ").map((e) => e.trim());
-    let sortedHand = hand
-      .split("")
-      .sort((a, b) => cards.indexOf(b) - cards.indexOf(a))
-      .join("");
-    const partsCount = getHandParts(sortedHand);
-    handsWithValues.push({
-      ...findHands(partsCount, hand),
-      hand: hand,
-      value: value,
-    });
+  instructions = inputAsRows[0].trim().split("");
+  let currentLocation = inputAsRows[2].split(" =")[0].trim();
+  inputAsRows.forEach((row, i) => {
+    if (!row || i === 0) return;
+    const [a, b] = row.split(" = ");
+    const location = a.trim();
+    const [L, R] = b
+      .trim()
+      .replace("(", "")
+      .replace(")", "")
+      .split(",")
+      .map((e) => e.trim());
+    map[location] = { R, L };
   });
-  const result = handsWithValues.sort((a, b) => {
-    return chooseWinner(a, b);
+  let i = 0;
+  // eslint-disable-next-line no-constant-condition
+  Object.entries(map).forEach(([key, value]) => {
+    let prevLocation = key;
+    for (i = 0; i < instructions.length; i++) {
+      prevLocation = map[prevLocation][instructions[i]];
+      map[key][instructions.slice(0, i + 1).join("")] = prevLocation;
+    }
   });
-
-  return result.reduce((acc, curr, i) => {
-    return acc + parseInt(curr.value) * (i + 1);
-  }, 0);
+  console.log(map);
+  // while (true) {
+  //   currentLocation = map[currentLocation].X;
+  //   if (currentLocation === "ZZZ") break;
+  //   i++;
+  // }
+  // return (i * instructions.length) / 2;
 }
 
-function secondTask(filename) {
-  const input = fs.readFileSync(`${day}/${filename}.txt`).toString();
-  const inputAsRows = input.split("\n");
-  const handsWithValues = [];
-  inputAsRows.forEach((row) => {
-    let [hand, value] = row.split(" ").map((e) => e.trim());
-    let sortedHand = hand
-      .split("")
-      .sort((a, b) => cards.indexOf(b) - cards.indexOf(a))
-      .join("");
-    const partsCount = getHandParts(sortedHand);
-    handsWithValues.push({
-      ...findHands(partsCount, hand),
-      hand: hand,
-      value: value,
-    });
-  });
-  const result = handsWithValues.sort((a, b) => {
-    return chooseWinner(a, b);
-  });
-
-  return result.reduce((acc, curr, i) => {
-    return acc + parseInt(curr.value) * (i + 1);
-  }, 0);
-}
-
-function findHands(hand, sorted) {
-  if (hand["5"]) {
-    return { rank: 7, faceValue: cards.indexOf(hand["5"][0]) };
-  }
-  if (hand["4"]) {
-    return {
-      rank: 6,
-      faceValue: hand["4"][0],
-    };
-  }
-  if (hand["3"] && hand["2"]) {
-    return {
-      rank: 5,
-      faceValue: hand["3"][0] + hand["2"][0],
-    };
-  }
-  if (hand["3"]) {
-    return {
-      rank: 4,
-      faceValue: hand["3"][0],
-    };
-  }
-  if (hand["2"]?.length === 2) {
-    return {
-      rank: 3,
-      faceValue: hand["2"][0] + hand["2"][1],
-    };
-  }
-  if (hand["2"]) {
-    return {
-      rank: 2,
-      faceValue: hand["2"][0],
-    };
-  }
-  hand = sorted.split("");
-  return { rank: 1, faceValue: "" };
-}
-
-function chooseWinner(hand1, hand2) {
-  if (hand1.rank !== hand2.rank) {
-    return hand1.rank - hand2.rank;
-  } else {
-    for (let i = 0; i < hand1.hand.length; i++) {
-      if (hand1.hand[i] !== hand2.hand[i]) {
-        return cards.indexOf(hand1.hand[i]) - cards.indexOf(hand2.hand[i]);
-      }
-    }
-  }
-}
-function getHandParts(hand) {
-  let prev = "";
-  let count = 0;
-  let parts = {};
-  let jokerCount = 0;
-
-  hand.split("").forEach((card) => {
-    if (card === "J") {
-      jokerCount++;
-      return;
-    }
-    if (prev === card) {
-      count++;
-      parts[card] = count;
-      return;
-    }
-    count = 1;
-    parts[card] = count;
-    prev = card;
-  });
-  if (jokerCount === 5) {
-    parts["A"] = 5;
-  } else if (jokerCount) {
-    const max = Math.max(...Object.values(parts));
-    const maxKey = Object.keys(parts).filter((key) => parts[key] === max);
-
-    if (maxKey.length === 1) {
-      parts[maxKey[0]] += jokerCount;
-    } else {
-      const maxKeyIndex = Math.max(...maxKey.map((key) => cards.indexOf(key)));
-      parts[cards[maxKeyIndex]] += jokerCount;
-    }
-  }
-
-  // transpose parts
-  const partsByCount = {};
-  Object.keys(parts).forEach((key) => {
-    const count = parts[key];
-
-    if (!partsByCount[count]) {
-      partsByCount[count] = [];
-    }
-    partsByCount[count].push(key);
-  });
-
-  // add joker count to otherw
-  return partsByCount;
-}
+function enrichLocation(currentLocation, i) {}

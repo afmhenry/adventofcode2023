b072eb4b10459938ace75de0ae430edc8c4af397
Alex <10469240+afmhenry@users.noreply.github.com>
2023-12-17 17:41:57 +0100

    stuck in loop, unsure why

---
+++ 16/task.js
@@ -0,0 +1,123 @@
+import fs from "fs";
+const day = 16;
+// result if coming from... [+x:[x,y], -x:[x,y], +y:[x,y], -y:[x,y]]
+const mirrors = {
+  "/": [[[0, -1]], [[0, 1]], [[-1, 0]], [[1, 0]]],
+  "\\": [[[0, 1]], [[0, -1]], [[1, 0]], [[-1, 0]]],
+  "|": [
+    [
+      [0, 1],
+      [0, -1],
+    ],
+    [
+      [0, 1],
+      [0, -1],
+    ],
+    [[0, 1]],
+    [[0, -1]],
+  ],
+  "-": [
+    [[1, 0]],
+    [[-1, 0]],
+    [
+      [1, 0],
+      [-1, 0],
+    ],
+    [
+      [1, 0],
+      [-1, 0],
+    ],
+  ],
+};
+
+// dx [-1,1]
+// x:
+function firstTask(filename) {
+  let result = 0;
+  const input = fs.readFileSync(`${day}/${filename}.txt`).toString();
+  const inputAsRows = input.split("\n");
+  const matrix = [];
+  inputAsRows.forEach((row) => {
+    if (!row) return;
+    matrix.push(row.split(""));
+  });
+  const temp = structuredClone(matrix);
+  // [x, y, xdelta, ydelta]
+  let current = [0, 0, 1, 0];
+
+  function reflect(state) {
+    const [x, y, dx, dy] = state;
+    const node = matrix?.[y]?.[x];
+    if (!node) return;
+    temp[y][x] = "#";
+
+    if (node === ".") {
+      if (!matrix?.[y + dy]?.[x + dx]) return;
+      return [[x + dx, y + dy, dx, dy]];
+    } else {
+      return directionToReflections(node, dx, dy).map((dir) => {
+        const [ndx, ndy] = dir;
+        if (!matrix?.[y + ndy]?.[x + ndx]) return;
+        return [x + ndx, y + ndy, ndx, ndy];
+      });
+    }
+  }
+  let prev = [current];
+  let i = 0;
+  while (true) {
+    let next = [];
+    prev.forEach((node) => {
+      if (!node) return;
+      let resp = reflect(node);
+      if (!resp) return;
+      next.push(...resp);
+    });
+    if (next.length === 0) break;
+    prev = next;
+    if (next.length > 1000000) break;
+  }
+  console.log(temp.map((e) => e.join("")).join("\n"));
+  return (
+    temp
+      .map((e) => e.join(""))
+      .join("")
+      .split("#").length - 1
+  );
+  return result;
+}
+
+function directionToReflections(node, dx, dy) {
+  if (dx === 1) return mirrors[node][0];
+  if (dx === -1) return mirrors[node][1];
+  if (dy === 1) return mirrors[node][2];
+  if (dy === -1) return mirrors[node][3];
+  return [];
+}
+
+function secondTask(filename) {
+  let result = 0;
+  const input = fs.readFileSync(`${day}/${filename}.txt`).toString();
+  const inputAsRows = input.split("\n");
+  inputAsRows.forEach((row) => {
+    if (!row) return;
+  });
+
+  return result;
+}
+
+function runTask(stage, filename, task, expected) {
+  console.log(`\nTask ${stage} - ${filename}`);
+  console.time(`\tTimer`);
+  const result = task(filename);
+  console.timeEnd(`\tTimer`);
+  console.log(`\tExpected ${expected}`);
+  console.log(`\tResult: ${result}`);
+  console.log(`\tPass: ${result === expected}\n`);
+}
+
+runTask("1", "sample", firstTask, 46);
+runTask("1", "full", firstTask, 18673);
+// 6361 too low
+// 7368 too low
+// runTask("2", "sample", secondTask, 2);
+// runTask("2", "full", secondTask, 948);

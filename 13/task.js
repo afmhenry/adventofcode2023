6b227e9f25260d47149c0fb470c64c7343c0e5a0
Alex <10469240+afmhenry@users.noreply.github.com>
2023-12-17 14:05:05 +0100

    cant solve part 2 of 13, got close..

--- 13/task.js
+++ 13/task.js
@@ -1,6 +1,19 @@
 import fs from "fs";
 const day = 13;
 
+const swap = {
+  "#": ".",
+  ".": "#",
+};
+function hasSmudge(a, b) {
+  b = b.split("");
+  a = a.split("");
+  const differences = a.filter((e, i) => b[i] !== e).length;
+  if (differences === 1) {
+    return a.map((e, i) => (e !== b[i] ? swap[e] : e)).join("");
+  }
+  return "";
+}
 function runTask(stage, filename, task, expected) {
   console.log(`\nTask ${stage} - ${filename}`);
   console.time(`\tTimer`);
@@ -13,12 +26,13 @@ function runTask(stage, filename, task, expected) {
 
 runTask("1", "sample", firstTask, 405);
 //runTask("1", "sample-2", firstTask, 405);
-runTask("1", "full", firstTask, 18673);
-// 25991 too low
-// 28927 too high
-// not 19223
-// runTask("2", "sample", secondTask, 2);
-// runTask("2", "full", secondTask, 948);
+runTask("1", "full", firstTask, 27300);
+runTask("2", "sample", secondTask, 400);
+runTask("2", "full", secondTask, 400);
+// 29167 too low
+// 31275 not
+// 31291 too high
+// 35326 too high
 
 function firstTask(filename) {
   let result = 0;
@@ -53,40 +67,95 @@ function transpose(matrix) {
 function findReflection(matrix, mult = 1) {
   let largestReflection = 0;
   for (var i = 0; i < matrix.length; i++) {
-    if (matrix[i] === matrix[i + 1]) {
-      let j = 0;
-      let reflection = false;
-
-      while (true) {
-        const [up, down] = [matrix[i + 1 + j], matrix[i - j]];
-        if (!up || !down) {
-          break;
-        }
-        if (up !== down) {
-          reflection = false;
-          break;
-        }
-        if (up === down) {
-          reflection = true;
-        }
-        j++;
+    let j = 0;
+    let reflection = false;
+    while (true) {
+      const [up, down] = [matrix[i + 1 + j], matrix[i - j]];
+      if (!up || !down) {
+        break;
       }
-      let reflectionVal = (i + 1) * mult;
-      if (reflection) {
-        //console.log(i, j, "\n" + matrix.slice(i - j, i + 1 + j).join("\n"));
-        largestReflection += reflectionVal;
+      if (up !== down) {
+        reflection = false;
+        break;
       }
+      if (up === down) {
+        reflection = true;
+      }
+      j++;
+    }
+    let reflectionVal = (i + 1) * mult;
+    if (reflection) {
+      //console.log(i, j, "\n" + matrix.slice(i - j, i + 1 + j).join("\n"));
+      largestReflection += reflectionVal;
     }
   }
   return largestReflection;
 }
+
 function secondTask(filename) {
   let result = 0;
   const input = fs.readFileSync(`${day}/${filename}.txt`).toString();
-  const inputAsRows = input.split("\n");
-  inputAsRows.forEach((row) => {
-    if (!row) return;
+  const sets = input.split("\n\n");
+  sets.forEach((set, num) => {
+    if (!set) return;
+    const matrix = set.split("\n"); //.map((e) => e.split(""));
+    let setResult = 0;
+    //console.log("rows", num);
+
+    setResult += findReflection2(matrix, 100);
+    const transposed = transpose(matrix.map((e) => e.split(""))).map((e) =>
+      e.join("")
+    );
+    //console.log("cols", num);
+    //console.log(transposed.join("\n"));
+    setResult += findReflection2(transposed, 1);
+    if (setResult) {
+      //console.log(num, setResult + "\n" + matrix.join("\n"));
+    }
+    result += setResult;
   });
 
   return result;
+}
+
+function findReflection2(matrix, mult = 1) {
+  let largestReflection = 0;
+  let matrixCopy = structuredClone(matrix);
+  for (var i = 0; i < matrix.length; i++) {
+    let smudgeUsed = false;
+    let j = 0;
+    let reflection = false;
+
+    while (true) {
+      const [up, down] = [matrix[i + 1 + j], matrix[i - j]];
+      if (!up || !down) {
+        break;
+      }
+      if (up !== down) {
+        if (!smudgeUsed) {
+          const newUp = hasSmudge(up, down);
+          if (newUp.length) {
+            //console.log("Found smudge", newUp, up, i, j);
+            matrix[i + 1 + j] = newUp;
+            smudgeUsed = true;
+            reflection = true;
+            continue;
+          }
+        }
+        matrix = matrixCopy;
+        reflection = false;
+        break;
+      }
+      if (up === down) {
+        reflection = true;
+      }
+      j++;
+    }
+    let reflectionVal = (i + 1) * mult;
+    if (reflection && smudgeUsed && !largestReflection) {
+      //console.log(reflectionVal, i, j, "\n" + matrix.join("\n"));
+      largestReflection += reflectionVal;
+    }
+  }
+  return largestReflection;
 }

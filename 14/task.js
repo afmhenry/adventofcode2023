5974e3484a2a208f881ba2e3b96b64dadab562af
Alex <10469240+afmhenry@users.noreply.github.com>
2023-12-17 14:20:26 +0100

    14 easy

--- 14/task.js
+++ 14/task.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-const day = 0;
+const day = 14;
 function transpose(matrix) {
   return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
 }
@@ -8,11 +8,29 @@ function firstTask(filename) {
   let result = 0;
   const input = fs.readFileSync(`${day}/${filename}.txt`).toString();
   const inputAsRows = input.split("\n");
+  const matrix = [];
   inputAsRows.forEach((row) => {
     if (!row) return;
+    matrix.push(row.split(""));
   });
+  const tMatrix = transpose(matrix);
+  //console.log(tMatrix.map((e) => e.join("")).join("\n"));
 
-  return result;
+  const shiftedTMatrix = tMatrix.map((row) => shiftStonesLeft(row));
+  const resultMatrix = transpose(shiftedTMatrix.map((e) => e.split("")));
+  console.log(resultMatrix.map((e) => e.join("")).join("\n"));
+  let load = 0;
+  resultMatrix.reverse().forEach((e, i) => {
+    load += e.filter((ee) => ee === "O").length * (i + 1);
+  });
+  return load;
+}
+function shiftStonesLeft(row) {
+  return row
+    .join("")
+    .split("#")
+    .map((e) => e.split("").sort().reverse().join(""))
+    .join("#");
 }
 
 function secondTask(filename) {
@@ -36,7 +54,7 @@ function runTask(stage, filename, task, expected) {
   console.log(`\tPass: ${result === expected}\n`);
 }
 
-runTask("1", "sample", firstTask, 114);
-// runTask("1", "full", firstTask, 18673);
+runTask("1", "sample", firstTask, 136);
+runTask("1", "full", firstTask, 18673);
 // runTask("2", "sample", secondTask, 2);
 // runTask("2", "full", secondTask, 948);

36a1453000c5654f9fd261c58197c3cdf758f29e
Alex <10469240+afmhenry@users.noreply.github.com>
2023-12-17 10:44:15 +0100

    stuck, again

--- 13/task.js
+++ 13/task.js
@@ -11,20 +11,70 @@ function runTask(stage, filename, task, expected) {
   console.log(`\tPass: ${result === expected}\n`);
 }
 
-runTask("1", "sample", firstTask, 114);
-// runTask("1", "full", firstTask, 18673);
+runTask("1", "sample", firstTask, 405);
+runTask("1", "sample-2", firstTask, 405);
+//runTask("1", "full", firstTask, 18673);
+// 25991 too low
+// 28927 too high
+// not 19223
 // runTask("2", "sample", secondTask, 2);
 // runTask("2", "full", secondTask, 948);
 
 function firstTask(filename) {
   let result = 0;
   const input = fs.readFileSync(`${day}/${filename}.txt`).toString();
-  const inputAsRows = input.split("\n");
-  inputAsRows.forEach((row) => {
-    if (!row) return;
+  const sets = input.split("\n\n");
+  sets.forEach((set, num) => {
+    if (!set) return;
+    const matrix = set.split("\n"); //.map((e) => e.split(""));
+    let setResult = 0;
+    setResult += findReflection(matrix, 100);
+    const transposed = transpose(matrix.map((e) => e.split(""))).map((e) =>
+      e.join("")
+    );
+    //console.log(transposed.join("\n"));
+    setResult += findReflection(transposed, 1);
+    if (setResult) {
+      console.log(num, setResult + "\n" + matrix.join("\n"));
+    }
+    result += setResult;
   });
 
   return result;
+}
+
+function transpose(matrix) {
+  return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+}
+
+function findReflection(matrix, mult = 1) {
+  let largestReflection = 0;
+  for (var i = 1; i < matrix.length; i++) {
+    if (matrix[i] === matrix[i + 1]) {
+      let j = 1;
+      let reflection = false;
+
+      while (true) {
+        const [up, down] = [matrix[i + 1 + j], matrix[i - j]];
+        if (!up || !down) break;
+        j++;
+
+        if (up === down) {
+          reflection = true;
+        }
+        if (up !== down) {
+          reflection = false;
+          break;
+        }
+      }
+      let reflectionVal = (i + 1) * mult;
+      if (reflection && largestReflection < reflectionVal) {
+        //console.log(i + 1, matrix[i + 1]);
+        largestReflection = reflectionVal;
+      }
+    }
+  }
+  return largestReflection;
 }
 function secondTask(filename) {
   let result = 0;
